307ce4b4c1b5004d601d8f8e79d8a94a5821f76b
cyspeclu <nellue.std1x59@gmail.com>
2023-10-09 22:58:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ echo "GO"
 wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
 sha256sum go1.20.5.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.21.2.linux-amd64.tar.gz
+sudo rm -rf go1.21.2.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
 source .zshrc
 
